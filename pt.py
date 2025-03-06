7ae14b4d945377d0ba12644cb4c35b9a2b5352ba
Rasyiq03 <mns.rasyiq03@gmail.com>
2025-03-06 17:14:30 +0700

    perbarui

--- pt.py
+++ pt.py
@@ -7,5 +7,14 @@ window.geometry('500x300')
 btn = tk.Button(window, text='Click me')
 btn.pack()
 
+btn2 = tk.Button(window, text='Click me')
+btn2.pack()
+
+btn3 = tk.Button(window, text='Click me')
+btn3.pack()
+
+btn1 = tk.Button(window, text='Click me')
+btn1.pack()
+
 
 window.mainloop()
